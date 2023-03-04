f5cbf07d0a963bb27cde9e0a91bc2b4938d43bd0
Mark Louie Espedido <espi@espidesigns.com>
2023-03-04 17:08:57 +0800

    adds paynamics ts

---
+++ src/functions/bin/paynamics.ts
@@ -0,0 +1,27 @@
+exports.handler = async function(event, context, callback) {   
+    // Get request's body
+    const request = JSON.parse(event.body)
+
+    // Validate that the request is coming from Snipcart
+    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
+
+    // Return a 404 if the request is not from Snipcart
+    if (!response.ok) return {
+        statusCode: 404,
+        body: ""
+    }
+
+    // Create a payment method list
+    let paymentMethodList = [{
+        id: 'Paynomics',
+        name: 'Paynamics 101',
+        iconUrl: '',
+        checkoutUrl: 'https://api.payserv.net/v1/rpf/transactions/rpf',
+    }]
+
+    // Return successful status code and available payment methods
+    return {
+        statusCode: 200,
+        body: JSON.stringify(paymentMethodList)
+    };
+}
