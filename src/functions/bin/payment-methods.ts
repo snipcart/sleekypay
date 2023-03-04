df33f70f9cab67fc0656d8f62feff5c59244b4bf
Mark Louie Espedido <espi@espidesigns.com>
2023-03-04 16:48:00 +0800

    check  event.body from aws-lambda

--- src/functions/bin/payment-methods.ts
+++ src/functions/bin/payment-methods.ts
@@ -15,8 +15,8 @@ if (!process.env.PRODUCTION) {
 
 exports.handler = async function (event: APIGatewayEvent, context: Context, callback: Callback) {
   // Get request's body
-  const trimmedBody = event.body.trim()
-  const request = JSON.parse(trimmedBody)
+  console.log('event.body', event.body)
+  const request = JSON.parse(event.body)
   const API_URL = process.env.API_URL || 'https://localhost:12666';
   const SITE_URL = process.env.URL || 'http://localhost:3000';
 
