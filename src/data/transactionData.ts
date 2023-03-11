50b71b1a30b782476d3851a36c410b19cf37e56a
Mark Louie Espedido <espi@espidesigns.com>
2023-03-11 18:13:02 +0800

    updates transactionData to grab from snipcart data

--- src/data/transactionData.ts
+++ src/data/transactionData.ts
@@ -1,5 +1,6 @@
-import { generate } from 'short-uuid';
+import { Context, Callback, APIGatewayEvent } from "aws-lambda";
 
+import { generate } from 'short-uuid';
 import { BillingInfo } from '../types/billingInfo';
 import { CustomerInfo } from '../types/customerInfo';
 import { OrderDetails } from '../types/orderDetails';
@@ -14,65 +15,75 @@ interface TransactionData {
   order_details: Partial<OrderDetails>;
 }
 
-export const transactionData: TransactionData = {
-  transaction: {
-    request_id: generate(),
-    notification_url: "https://www.paynamics.com/notify",
-    response_url: "https://www.paynamics.com/response",
-    cancel_url: "https://www.paynamics.com/cancel",
-    pchannel: "",
-    pmethod: "",
-    collection_method: "single_pay",
-    payment_notification_status: "1",
-    payment_notification_channel: "1",
-    amount: "111.50",
-    currency: "USD",
-    trx_type: "sale",
-  },
-  billing_info: {
-    billing_address1: "First Street",
-    billing_address2: "H.V. dela Costa Street",
-    billing_city: "Makati",
-    billing_state: "Metro Manila",
-    billing_country: "PH",
-    billing_zip: "1227"
-  },
-  shipping_info: {
-    shipping_address1: "First Street",
-    shipping_address2: "",
-    shipping_city: "Quezon City",
-    shipping_state: "Metro Manila Area",
-    shipping_country: "PH",
-    shipping_zip: "1229"
-  },
-  customer_info: {
-    fname: "Jan",
-    lname: "Dae",
-    mname: "",
-    email: "jandae@gmal.com",
-    phone: "09171234567",
-    mobile: "09171234567",
-    dob: "",
-  },
-  order_details: {
-    orders: [
-      {
-        itemname: "Test Product",
-        quantity: 1,
-        unitprice: "101.50",
-        totalprice: "101.50"
-      },
-      {
-        itemname: "Convenience Fee",
-        quantity: 1,
-        unitprice: "10.00",
-        totalprice: "10.00",
-        servicecharge: true
-      }
-    ],
-    subtotalprice: "111.50",
-    shippingprice: "0.00",
-    discountamount: "0.00",
-    totalorderamount: "111.50"
+console.log('before payload');
+exports.handler = async function (event: APIGatewayEvent, context: Context, callback: Callback) {
+  console.log('event.body', event.body);
+  const requestBody = JSON.parse(event.body);
+  console.log('requestBody', requestBody);
+
+  let transactionData: TransactionData = {
+
+    transaction: {
+      request_id: generate(),
+      notification_url: "https://www.paynamics.com/notify",
+      response_url: "https://www.paynamics.com/response",
+      cancel_url: "https://www.paynamics.com/cancel",
+      pchannel: "",
+      pmethod: "",
+      collection_method: "single_pay",
+      payment_notification_status: "1",
+      payment_notification_channel: "1",
+      amount: "111.50",
+      currency: requestBody.invoice.currency,
+      trx_type: "sale",
+    },
+    billing_info: {
+      billing_address1: requestBody.invoice.billingAddress.streetAndNumber,
+      billing_address2: "H.V. dela Costa Street",
+      billing_city: "Makati",
+      billing_state: "Metro Manila",
+      billing_country: "PH",
+      billing_zip: "1227"
+    },
+    shipping_info: {
+      shipping_address1: requestBody.invoice.billingAddress.streetAndNumber,
+      shipping_address2: "",
+      shipping_city: "Quezon City",
+      shipping_state: "Metro Manila Area",
+      shipping_country: "PH",
+      shipping_zip: "1229"
+    },
+    customer_info: {
+      fname: "Jan",
+      lname: "Dae",
+      mname: "",
+      email: "jandae@gmal.com",
+      phone: "09171234567",
+      mobile: "09171234567",
+      dob: "",
+    },
+    order_details: {
+      orders: [
+        {
+          itemname: "Test Product",
+          quantity: 1,
+          unitprice: "101.50",
+          totalprice: "101.50"
+        },
+        {
+          itemname: "Convenience Fee",
+          quantity: 1,
+          unitprice: "10.00",
+          totalprice: "10.00",
+          servicecharge: true
+        }
+      ],
+      subtotalprice: "111.50",
+      shippingprice: "0.00",
+      discountamount: "0.00",
+      totalorderamount: "111.50"
+    }
   }
+
+  return transactionData;
 }
